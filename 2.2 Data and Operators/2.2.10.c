ef7927788e55503cc442ef4c4d9936f0d2ab9e8e
SeriousPetrovich <egorov.peta1997@mail.ru>
2016-03-31 00:30:53 +0300

    Story 37: Adding 2.2.10

---
+++ 2.2 Data and Operators/2.2.10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <float.h>
+int main(void) 
+{
+	printf("Number of bytes in:\n\n");
+	printf("Char         	   = 1;\n");
+	printf("Signed char   	   = 1;\n");
+	printf("Unsigned char 	   = 1;\n\n");
+	printf("Short        	   = 2;\n");
+	printf("Unsigned short	   = 2;\n");
+	printf("Signed short  	   = 2;\n\n");
+	printf("Int          	   = 2 or 4;\n");
+	printf("Unsigned int 	   = 2 or 4;\n");
+	printf("Signed int	   = 2 or 4;\n\n");
+	printf("Long               = 4;\n");
+	printf("Unsigned long      = 4;\n");
+	printf("Signed long        = 4;\n\n");
+	printf("Long long          = 8;\n");
+	printf("Unsigned long long = 8;\n");
+	printf("Signed long long   = 8;\n\n");
+	printf("Float		   = 4;\n");
+	printf("Double 		   = 8;\n");
+	printf("Long double 	   = 10.\n");
+	return 0;
+}
+
+
